0c203a32a59de375e353977be879eed6aea0a981
dudagiglioli <dudaguilice@gmail.com>
2023-09-21 09:00:25 -0300

    error

---
+++ src/pages/Esmaltes/Filtros/index.tsx
@@ -0,0 +1,20 @@
+import styles from './Filtros.module.scss';
+import filtros from './filtros.json'
+
+
+type Iopcao = typeof filtros[0];
+
+export default function Filtros(){
+    return(
+        <div className={styles.filtro}>
+            {
+                filtros.map((opcao) => (
+                    <button className={styles.filtro__filtro} key={opcao.id}>
+                        {opcao.label}
+                    </button>
+                )
+                )
+            }
+        </div>
+    );
+}
--- src/pages/Esmaltes/index.tsx
+++ src/pages/Esmaltes/index.tsx
@@ -1,14 +1,37 @@
 import styles from "./Esmalte.module.scss";
-import Logo from "assets/logo";
+import Logo from "../../assets/logo.png";
+import Buscador from "./Buscador"
+import { useState } from "react";
+import Filtros from "./Filtros";
 
 
 export default function Esmalte() {
+
+    const [busca, setBusca] = useState("");
+
  return(
 <main>
 
 <nav className="styles.inicio">
     <img src={Logo} alt="Logo Esmalteria"/>
 </nav>
+
+<header className={styles.header}>
+<div className={styles.header__text}>
+    "Esmalteria!"
+</div>
+           </header>
+
+           <section className={styles.esmalte}>
+               <h3 className={styles.esmalte__titulo}>Opções</h3>
+               
+           <Buscador busca={busca} setBusca={setBusca}></Buscador>
+
+           <div className='styles.esmalte__filtros'> 
+                <Filtros/>
+           </div>
+           </section>
+
 
 </main>
 
